a4abf7750cf05b0865108c064fc04247669836b0
Imanghader <49917769+iman008@users.noreply.github.com>
2024-01-30 02:28:12 +0330

    run.bat updated

--- run.bat
+++ run.bat
@@ -1,7 +1,4 @@
 cd build/
-if (Test-Path CMakeCache.txt) {
-    # If it exists, run cmake --build . --target clean
-    cmake --build . --target clean
-}
+cmake --build . --target clean
 cmake --build  .
 Debug\main.exe 
